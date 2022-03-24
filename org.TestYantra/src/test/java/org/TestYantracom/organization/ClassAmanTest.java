dda4e8a5c6a8f5a6f7c6a5199d78e013848b24df
Lenovo <Lenovo@Aman-Anand>
2022-03-24 15:03:34 +0530

    changes done by aman

---
+++ org.TestYantra/src/test/java/org/TestYantracom/organization/ClassAmanTest.java
@@ -0,0 +1,13 @@
+package org.TestYantracom.organization;
+
+import org.testng.annotations.Test;
+
+public class ClassAmanTest {
+	@Test
+	public void createAman() {
+		System.out.println("Aman created sucesfully");
+	}
+	
+	
+
+}
