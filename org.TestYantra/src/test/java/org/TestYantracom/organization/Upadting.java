337d51531b095f898ceb0f72741fb2878dd4f365
aviba <aviba@DESKTOP-9MKM63L>
2022-03-24 14:24:32 +0530

    Update of first commit

---
+++ org.TestYantra/src/test/java/org/TestYantracom/organization/Upadting.java
@@ -0,0 +1,12 @@
+package org.TestYantracom.organization;
+
+import org.testng.annotations.Test;
+
+public class Upadting {
+
+	@Test
+	
+	public void updation() {
+		System.out.println("This is the Updation");
+	}
+}
